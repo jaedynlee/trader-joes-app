7efcc85c0961579a8f113aeaa2fd21143d79371d
Jaedyn Lee <jaedyn11@gmail.com>
2022-10-03 23:10:48 -0400

    Add tab navigation

--- App.js
+++ App.js
@@ -1,40 +1,80 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
 import AllProductsList from "./components/ProductsList/allProductsList";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { colors } from "./style";
 import ProductDetails from "./components/ProductDetails/productDetails.js";
 import FilteredProductsList from "./components/ProductsList/filteredProductsList";
+import Settings from "./components/Settings/settings";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faAppleWhole,
+  faGear,
+  faListCheck,
+} from "@fortawesome/free-solid-svg-icons";
 
-const Stack = createNativeStackNavigator();
+const ProductStack = createNativeStackNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Stack.Navigator>
-          <Stack.Screen
-            name="All Products List"
-            component={AllProductsList}
-            options={{ title: "All Products" }}
-          />
-          <Stack.Screen
-            name="Filtered Products List"
-            component={FilteredProductsList}
-            options={({ route }) => ({ title: route.params.name })}
-          />
-          <Stack.Screen
-            name="Product Details"
-            component={ProductDetails}
-            options={({ route }) => ({ title: route.params.name })}
-          />
-        </Stack.Navigator>
-      </View>
-    </NavigationContainer>
-  );
-}
+const Tab = createBottomTabNavigator();
+
+const ProductStackNavigator = () => (
+  <ProductStack.Navigator>
+    <ProductStack.Screen
+      name="All Products List"
+      component={AllProductsList}
+      options={{ title: "All Products" }}
+    />
+    <ProductStack.Screen
+      name="Filtered Products List"
+      component={FilteredProductsList}
+      options={({ route }) => ({ title: route.params.name })}
+    />
+    <ProductStack.Screen
+      name="Product Details"
+      component={ProductDetails}
+      options={({ route }) => ({ title: route.params.name })}
+    />
+  </ProductStack.Navigator>
+);
+
+export default App = () => (
+  <NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Products"
+          component={ProductStackNavigator}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcon icon={faAppleWhole} color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Shopping List"
+          component={Settings}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcon icon={faListCheck} color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcon icon={faGear} color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </SafeAreaView>
+  </NavigationContainer>
+);
 
 const styles = StyleSheet.create({
   container: {
