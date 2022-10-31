9593a3c67b46f698b58321bcbf8804f419925733
Jaedyn Lee <jaedyn11@gmail.com>
2022-10-30 21:13:21 -0400

    Fix ingredients formatting

--- components/ProductDetails/ingredients.js
+++ components/ProductDetails/ingredients.js
@@ -10,14 +10,26 @@ const styles = StyleSheet.create({
 });
 
 const Ingredients = ({ ingredients, allergens }) => {
+  // Ensure ingredients list ends in a period `.`
+  if (ingredients.length) {
+    const lastIngredient = ingredients.pop();
+    const lastCharacter = lastIngredient.ingredient.trim().slice(-1);
+    if (lastCharacter !== ".") {
+      lastIngredient.ingredient = lastIngredient.ingredient.trim() + ".";
+    }
+    ingredients.push(lastIngredient);
+  }
   return (
     <View style={styles.container}>
       <CollapsibleDetails label="Ingredients">
         <BodyText>
-          {ingredients.map((i) => i.ingredient.trim()).join(", ")}
+          {ingredients
+            .map((i) => i.ingredient.trim())
+            .join(", ")
+            .trim()}
           <BodyText style={{ fontWeight: "bold" }}>
-            {" "}
-            {allergens && allergens.map((a) => a.ingredient.trim()).join(" ")}
+            {allergens &&
+              ` ${allergens.map((a) => a.ingredient.trim()).join(" ")}`}
           </BodyText>
         </BodyText>
       </CollapsibleDetails>
